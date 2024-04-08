de0f12c9c4b9eebc8b01d30114b56ec5cc35b474
minesp3164 <lhd000721@gmail.com>
2024-04-08 21:46:14 +0900

    feat: footer와 header 생성 및 팀원 전원 표시

---
+++ frontend/src/page/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () =>{
+  return(
+    <div className="text-center text-8xl">
+      사진
+    </div>
+  )
+}
+export default Header;
---
+++ frontend/src/page/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Start from "../component/start"
+import Header from "./header";
+import Footer from "./footer";
+const Main = () =>{
+  return(
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+};
+
+export default Main;

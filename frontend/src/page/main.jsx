76664b138279fae2c801f7aceff2e08826529b58
minesp3164 <lhd000721@gmail.com>
2024-04-09 20:32:36 +0900

    feat: jsx파일의 위치 조정 및 main 페이지 -> start페이지 이동

--- frontend/src/page/main.jsx
+++ frontend/src/page/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import Start from "../component/start"
-import Header from "./header";
-import Footer from "./footer";
+import Header from "../component/header";
+import Footer from "../component/footer";
+import {useNavigate} from "react-router-dom";
 const Main = () =>{
+  const navigate = useNavigate();
+  const goToNextPage = () =>{
+    navigate('/start');
+  }
   return(
     <>
       <Header/>
+      <button onClick={goToNextPage}>시작하시려면 절 눌러주세요</button>
       <Footer/>
     </>
   )

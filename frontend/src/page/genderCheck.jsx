e075ecec1d1e03d24b07c22434d4e1100cb6d92e
minesp3164 <lhd000721@gmail.com>
2024-04-11 22:20:09 +0900

    feat: 전반적인 수정작업 및 axios install

--- frontend/src/page/genderCheck.jsx
+++ frontend/src/page/genderCheck.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const GenderCheck = () =>{
   const navigate = useNavigate();
-  const [gender, setGender] = React.useState();
+  const [userGender, setUserGender] = React.useState();
+
   const onClick = (e) =>{
     console.log(e.target.id)
-    setGender(e.target.id)
+    setUserGender(e.target.id)
   }
+
+  const ShowGender = () => {
+    if (userGender === 'female'){
+      return <div>당신의 성별은 여성입니다.</div>
+    }
+    else if(userGender ==='male'){
+      return <div>당신의 성별은 남성입니다.</div>
+    }
+    else{
+      return <div>성별을 선택해 주세요.</div>
+    }
+  }
+
   const goToUploadPage = () =>{
     navigate('/upLoad')
   }
+
   return(
     <div>
       성별 확인 페이지입니다.
       <h2> 당신의 성별을 알려주세요</h2>
-      <button id="여성" onClick={onClick}>여성</button>
-      <button id="남성" onClick={onClick}>남성</button>
-      <h2>당신의 성별은{gender}입니다</h2>
-      <button disabled={!gender} onClick={goToUploadPage}>다음으로</button>
+      <button id="female" onClick={onClick}>여성</button>
+      <button id="male" onClick={onClick}>남성</button>
+      <ShowGender/>
+      <button disabled={!userGender} onClick={genderCheck}>다음으로</button>
     </div>
   )
 }
--- frontend/src/page/main.jsx
+++ frontend/src/page/main.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../component/header";
-import Footer from "../component/footer";
 import {useNavigate} from "react-router-dom";
 const Main = () =>{
   const navigate = useNavigate();
@@ -9,9 +7,7 @@ const Main = () =>{
   }
   return(
     <>
-      <Header/>
       <button onClick={goToNextPage}>시작하시려면 절 눌러주세요</button>
-      <Footer/>
     </>
   )
 };

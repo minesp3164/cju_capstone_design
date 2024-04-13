473c3d78ca7103f6c6418e6818842b00f426e8cd
minesp3164 <lhd000721@gmail.com>
2024-04-13 22:13:03 +0900

    feat: footer의 css 업데이트

--- frontend/src/component/footer.jsx
+++ frontend/src/component/footer.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import devData from "../data/devData.json";
 
 const Footer = () => {
-  return(
-    <div className="grid place-items-center bg-cyan-900 " >
-      {devData.users.map(
-        user => (
-          <div className="p-2 text-amber-50">{user.name}
-            <a href={user.gitAddr}><i className="fa-brands fa-github"/></a>
-          </div>
-        ))}
+  return (
+    <div className="relative h-32 w-full my-2 md:mx-auto bg-cyan-900 items-center" >
+      <div className="absolute grid gird-cols-2 bottom-0 inset-x-10 inset-y-5 items-center">
+        <div className="">
+          사진의 마술사
+        </div>
+        <div className="h-16">
+          {devData.users.map(
+            user => (
+              <ul className=" px-2 text-amber-50">{user.name}
+                <a href={user.gitAddr}><i className="fa-brands fa-github"/></a>
+              </ul>
+            ))}
+        </div>
+        <div className= "col-span-2  px-20 bottom-0">
+          cju
+        </div>
+      </div>
     </div>
   )
 }

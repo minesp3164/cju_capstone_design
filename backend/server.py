d9061952d4f5ca19d300aaa2e32c8af2bdb8a4dd
minesp3164 <lhd000721@gmail.com>
2024-04-15 20:07:50 +0900

    feat: backend서버에  gender 데이터 수신 및 응답

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
 import base64
 
 
@@ -6,6 +6,19 @@ app = Flask(__name__)
 
 
 
+
+
+@app.route('/genderCheck', methods=['POST', 'OPTIONS'])
+def genderCheck():
+    if request.method == 'POST':
+        gender_data = request.json
+        return jsonify(gender_data)
+    elif request.method == 'OPTIONS':  # OPTIONS 메서드에 대한 응답 추가
+        response = app.make_default_options_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'  # CORS 정책 허용
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        return response
 
 
 

c9467251e1ccaec9d95b40906413b01d20e6ab4d
minesp3164 <lhd000721@gmail.com>
2024-04-24 22:23:57 +0900

    feat:프론트로 보내는 파일 이름 수정

--- backend/server.py
+++ backend/server.py
@@ -15,18 +15,18 @@ picture = None
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
-        file = request.files['file']  # 'file'이라는 키로 파일 가져오기
+        file = request.files['file']
         file.save("image/test.jpg")
         return jsonify({'filename': file.filename})
     except KeyError:
 
-        return jsonify({'error': 'No file part'}), 400  # 'file' 키가 없는 경우 에러 응답
+        return jsonify({'error': 'No file part'}), 400
 
 
 @app.route('/result', methods=['GET', 'POST'])
 def get_image():
     try:
-        with open('image/1.jpg', 'rb') as img_file:
+        with open('image/test.jpg', 'rb') as img_file:
             encoded_image = base64.b64encode(img_file.read()).decode('utf-8')
         return jsonify({'image': encoded_image})
     except FileNotFoundError:

2583a8c0f79aaa579eda6eba6b08a34b18b13440
minesp3164 <lhd000721@gmail.com>
2024-04-30 21:44:21 +0900

    feat: 프로젝트명 교체

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './input.css';
-import Body from "./component/Body";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
+  import Body from "./component/Body";
+  import Header from "./component/Header";
+  import Footer from "./component/Footer";
 
 const App = () => {
   return (

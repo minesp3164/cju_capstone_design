96be11e83b4c93ba328a8f98e42c6b133739beee
minesp3164 <lhd000721@gmail.com>
2024-04-30 21:41:36 +0900

    feat: 성별 구분 및 얼굴형 분석 파일 리팩토링 및 함수화

--- backend/gender_classification.py
+++ backend/gender_classification.py
@@ -1,23 +1,18 @@
 from transformers import pipeline
 from PIL import Image
 
-# 이미지 분류 모델 불러오기
-pipe = pipeline("image-classification", model="rizvandwiki/gender-classification")
 
-# 이미지 파일 불러오기
-image_path = "image/image01.jpg"
-image = Image.open(image_path)
+def gender_classify(image):
+    pipe = pipeline("image-classification", model="rizvandwiki/gender-classification")
 
-# 이미지 분류 수행
-result = pipe(image)
 
-predicted_label = result[0]['label']
+    result = pipe(image)
 
-# 성별
-if predicted_label == 'male':
-    predicted_label = '남성'
-elif predicted_label == 'female':
-    predicted_label = '여성'
+    predicted_label = result[0]['label']
 
-# 결과 출력
-print(predicted_label)
+    gender = {
+        'male': '남성',
+        'female': '여성',
+    }
+    # 성별
+    return gender[predicted_label]

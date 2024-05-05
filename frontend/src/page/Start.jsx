ab79032159263e6830bc74eaae9ca1b02221e047
minesp3164 <lhd000721@gmail.com>
2024-05-06 00:55:49 +0900

    refactor: 서비스 이용 페이지 UX 개선

--- frontend/src/page/Start.jsx
+++ frontend/src/page/Start.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
-import { Button } from "react-daisyui";
+import { Button, Checkbox } from "react-daisyui";
 
 
 const Start = () => {
@@ -17,8 +17,13 @@ const Start = () => {
     <div className="">
       <div className="text-center font-bold lg:text-5xl sm:text-2xl pb-10 flex justify-center items-center gap-x-[15px]">
 
-        <input className="accent-amber-300 form-checkbox lg:h-8 lg:w-8 sm:h-6 sm:w-6" type="checkbox" id="checkbox" onChange={() => setService(prev => !prev)}/>
-        <label for="checkbox" className="lg:text-4xl sm:text-2xl text-white font-medium">서비스를 이용하시려면 눌러주세요</label><p/>
+        <Checkbox
+          checked={service}
+          id="checkbox"
+          onChange={() => setService(prev => !prev)}
+          className="bg-white"
+        />
+        <label for="checkbox" className="lg:text-4xl sm:text-2xl text-white font-medium cursor-pointer">서비스를 이용하시려면 눌러주세요</label><p/>
       </div>
       <div className="text-center ">
         <Button

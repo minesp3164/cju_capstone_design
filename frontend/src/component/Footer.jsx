9fc1250758b9dd3f33718913ce0ac6675d0cdccc
minesp3164 <lhd000721@gmail.com>
2024-05-06 00:24:56 +0900

    style: 푸터 스타일링

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -4,23 +4,25 @@ import devData from "../data/devData.json";
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full">
-      <div className="relative h-32 md:mx-auto bg-gray-100 items-center" >
+      <div className="relative h-32 md:mx-auto bg-stone-800 items-center" >
         <div className="absolute grid gird-cols-2 bottom-0 inset-x-10 inset-y-5 items-center">
-          <div id="팀 로고" className="text-xl font-bold">
+          <div id="팀 로고" className="text-xl font-bold text-slate-50">
             사진의 마술사
           </div>
           <div className="h-16">
             <div className="text-sm text-right">
-              <div className="text-xl">참여한 개발자</div>
+              <div className="text-xl text-stone-200">참여한 개발자</div>
               {devData.users.map(
                 user => (
-                  <ul className="px-2 text-gray-600">{user.name}
+                  <div>
+                  <ul className="px-2 text-stone-400">{user.name}
                     <a href={user.gitAddr}><i className="fa-brands fa-github"/></a>
                   </ul>
+                  </div>  
                 ))}
             </div>
           </div>
-          <div className= "col-span-2 px-40 bottom-0 text-gray-400">
+          <div className= "col-span-2 px-40 bottom-0 text-stone-300">
             projected by CJU
           </div>
         </div>
@@ -28,4 +30,5 @@ const Footer = () => {
     </div>
   )
 }
+
 export default Footer;

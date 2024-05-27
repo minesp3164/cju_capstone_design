8b9b1f0c60e60e3d0fd13f652081927a97f294f8
minesp3164 <lhd000721@gmail.com>
2024-05-27 23:13:53 +0900

    feat: 주석 달기 및 사람 확인 함수 추가

--- backend/recommend/face_shape_classification.py
+++ backend/recommend/face_shape_classification.py
@@ -4,8 +4,40 @@ import random
 from transformers import pipeline
 from .data import recommendations
 from .gender_classification import get_gender
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
 
 
+#사람 인지 아닌지 혹은 한명인지
+def get_is_person(image):
+    # 모델과 프로세서 로드
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+    # 입력 데이터 준비
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    # 출력 데이터를 COCO API 형식으로 변환
+    # 스코어가 0.9 이상인 감지만 유지
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    # 결과 출력
+    is_person = False
+    num_people = 0
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        label_name = model.config.id2label[label.item()]
+        if label_name == 'person':
+            num_people += 1
+
+    if num_people == 1:
+        is_person = True
+
+    return is_person
+
+#얼굴형의 종류
 shape = {
     "Round": "둥근형 얼굴",
     "Heart": "하트형 얼굴",
@@ -19,6 +51,7 @@ def rand(n):
     return random.randint(0, n - 1)
 
 
+#얼굴형 확인하기
 def get_face_shape(image: str):
     # 이미지 분류 모델 불러오기
     pipe = pipeline("image-classification", model="metadome/face_shape_classification")
@@ -27,6 +60,7 @@ def get_face_shape(image: str):
     return max(predicted_labels, key=lambda x: x[1])[0] # Round, Heart, Oblong, Square, Oval
 
 
+#헤어스타일
 def get_recommend_hairstyle(image):
     face_name: str = get_face_shape(image)
     gender = get_gender(image)
@@ -40,8 +74,10 @@ def get_recommend_hairstyle(image):
             recommends
         )
     )
+    # is_person:bool = get_is_person(im+age)
     item_len = len(results)
     result = results[rand(item_len)]
+    # result['is_person'] = is_person
     result['shape'] = shape_name
 
     return result

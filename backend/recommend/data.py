2062f2dbcf3e86eb20ec7aee0460c8d983676af6
zopc1548 <zopc1314@gmail.com>
2024-06-02 16:43:36 +0900

    feat: 여자 헤어스타일 설명 추가

--- backend/recommend/data.py
+++ backend/recommend/data.py
@@ -18,16 +18,16 @@ round_face_recommendations: List[Dict[str, str]] = [
     {'name': "투 블럭 컷", 'desc': '단발머리 아래쪽 절반을 이발기로 다듬어 위쪽 절반을 그대로 남겨 턱이 생기도록 한 헤어스타일입니다. 앞머리와 윗머리는 남기고 옆 뒷머리를 짧게 치는 헤어스타일로, 머리가 두 부분으로 나뉜다고 해서 투 블록이라고 불립니다.', 'sex': 'male', 'path': '../image/male_hairstyle/round/two_block.jpg'},
     {'name': "언더 컷", 'desc': '머리 전체를 일정한 길이로 깎은 후, 머리의 하부나 한쪽 옆부분을 매우 짧게 깎아 스타일링하는 헤어컷 스타일입니다. 이 스타일은 긴 윗머리와 짧은 옆머리의 대조가 독특하며, 현대적이고 개성 있는 이미지를 연출합니다.', 'sex': 'male', 'path': '../image/male_hairstyle/round/under_cut.jpg'},
     {'name': '롱 레이어드', 'desc': '양쪽으로 긴 레이어를 넣어 얼굴을 길게 보이도록 하는 스타일입니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/long_layered_cut.jpg'},
-    {'name': "버드 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/bird_cut.jpg'},
-    {'name': "그레이스 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/grace_perm.jpg'},
-    {'name': "허그 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/hug_cut.jpg'},
-    {'name': "허쉬 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/hush_cut.jpg'},
-    {'name': "미스티 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/misty_perm.jpg'},
+    {'name': "버드 컷", 'desc': '목선 위로 짧게 자른 헤어스타일로, 새의 깃털처럼 가볍고 날렵한 느낌을 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/bird_cut.jpg'},
+    {'name': "그레이스 펌", 'desc': '부드럽고 우아한 웨이브를 연출하는 헤어스타일로, 자연스럽게 흐르는 컬이 특징입니다. 이 스타일은 고급스러우면서도 로맨틱한 분위기를 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/grace_perm.jpg'},
+    {'name': "허그 컷", 'desc': '얼굴을 감싸는 듯한 느낌을 주는 헤어스타일로, 앞머리를 길게 자르고 옆머리를 짧게 깎아 층을 내어 자연스럽고 부드러운 느낌을 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/hug_cut.jpg'},
+    {'name': "허쉬 컷", 'desc': '부드럽고 자연스러운 곱슬을 연출하는 헤어스타일로, 머리에 볼륨과 우아함을 부여합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/hush_cut.jpg'},
+    {'name': "미스티 펌", 'desc': '부드럽고 우아한 웨이브를 연출하는 헤어스타일로, 자연스러운 느낌과 섬세한 포인트를 강조합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/misty_perm.jpg'},
     {'name': "롱 보브", 'desc': '길이가 있는 보브 컷은 머리카락이 얼굴을 감싸면서 얼굴을 좀 더 슬림하게 보이게 해줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/long_bob_cut.jpg'},
-    {'name': "클라우드 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/cloud_perm.jpg'},
+    {'name': "클라우드 펌", 'desc': '부드럽고 우아한 웨이브를 연출하는 헤어스타일로, 공중에 떠 있는 구름처럼 부드러운 느낌을 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/cloud_perm.jpg'},
     {'name': "웨이브 펌", 'desc': '약간의 웨이브를 넣은 스타일은 둥근 얼굴형에 잘 어울리며, 얼굴을 길어 보이게 해줄 수 있어요.', 'sex': 'common', 'path': '../image/female_hairstyle/round/wave_perm.jpg'},
-    {'name': "볼륨 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/volume_perm.jpg'},
-    {'name': "에그 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/round/egg_perm.jpg'}
+    {'name': "볼륨 펌", 'desc': '머리카락에 볼륨과 우아함을 부여하는 헤어스타일로, 부드럽고 우아한 곡선을 연출합니다. 자연스러운 웨이브와 풍성한 볼륨을 강조하여 머리를 풍성하게 보이게 합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/volume_perm.jpg'},
+    {'name': "에그 펌", 'desc': '부드러운 곡선과 자연스러운 볼륨감을 강조한 웨이브 펌으로, 달걀형 얼굴처럼 부드럽고 둥근 느낌을 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/round/egg_perm.jpg'}
 ]
 
 
@@ -43,15 +43,15 @@ heart_face_recommendations: List[Dict[str, str]] = [
     {'name': "볼륨 펌", 'desc': '머리카락에 볼륨과 높이를 주어 풍성하고 화려한 느낌을 연출하는 헤어스타일입니다. 볼륨컷은 자신감 있고 세련된 느낌을 주며, 다양한 머리 스타일과 잘 어울리는 헤어컷 중 하나입니다.', 'sex': 'male', 'path': '../image/male_hairstyle/heart/volume_perm.jpg'},
     {'name': '롱 레이어드', 'desc': '양쪽으로 긴 레이어를 넣어 얼굴을 길게 보이도록 하는 스타일입니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/long layered_cut.jpg'},
     {'name': "롱 스트레이트", 'desc': '약간의 웨이브를 넣은 스타일은 둥근 얼굴형에 잘 어울리며, 얼굴을 길어 보이게 해줄 수 있어요.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/long_straight_cut.jpg'},
-    {'name': '레이어드 보브', 'desc': '다양한 레이어를 넣어 보브 컷을 스타일링하면 얼굴의 형태를 부드럽게 보정해줄 수 있습니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/layered_bob_cut.jpg'},
-    {'name': "벌룬 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/balloon_perm.jpg'},
-    {'name': "보브 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/bob_cut.jpg'},
-    {'name': "보더 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/boder.jpg'},
-    {'name': "레아 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/lea_perm.jpg'},
-    {'name': "러플 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/ruffle_perm.jpg'},
+    {'name': '레이어드 보브', 'desc': '턱선 길이의 보브 스타일에 여러 층을 추가하여 볼륨과 움직임을 더하는 헤어스타일입니다. 이 스타일은 얼굴을 작아 보이게 하며, 세련되고 현대적인 이미지를 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/layered_bob_cut.jpg'},
+    {'name': "벌룬 펌", 'desc': '풍성하고 볼륨감 있는 웨이브를 연출하는 헤어스타일로, 얼굴을 작아 보이게 하고 부드러운 이미지를 강조합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/balloon_perm.jpg'},
+    {'name': "보브 컷", 'desc': '어깨 라인을 기준으로 길이가 짧게 자른 헤어스타일로, 앞머리와 뒷머리의 길이가 비슷하게 유지됩니다. ', 'sex': 'female', 'path': '../image/female_hairstyle/heart/bob_cut.jpg'},
+    {'name': "보더 펌", 'desc': '부드럽고 자연스러운 웨이브를 연출하는 헤어스타일로, 헤어 끝에 경사를 주어 자연스러운 곡선을 형성합니다. 부드럽고 우아한 느낌을 주며, 머리카락을 풍성하게 보이게 합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/boder.jpg'},
+    {'name': "레아 펌", 'desc': '자연스러운 컬과 볼륨감을 강조한 헤어스타일로, 얼굴 윤곽을 부드럽게 보이게 하고 우아하면서도 캐주얼한 느낌을 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/lea_perm.jpg'},
+    {'name': "러플 펌", 'desc': '부드럽고 자연스러운 웨이브를 강조하는 헤어스타일로, 머리카락을 부드럽게 풀어 풍성한 볼륨을 연출합니다. ', 'sex': 'female', 'path': '../image/female_hairstyle/heart/ruffle_perm.jpg'},
     {'name': "풀 보브", 'desc': '양쪽으로 볼륨 있는 보브 컷은 얼굴의 하트 모양을 부드럽게 잡아주면서 매력을 더해줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/full_bob.jpg'},
-    {'name': "미디움 레이어드 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/midium_layered_cut.jpg'},
-    {'name': "에그 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/heart/egg_perm.jpg'}
+    {'name': "미디움 레이어드 컷", 'desc': '중간 길이의 머리카락에 층을 내어 자연스럽고 부드러운 웨이브를 연출하는 헤어스타일입니다. 다양한 얼굴형에 어울리며, 세련되고 현대적인 느낌을 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/midium_layered_cut.jpg'},
+    {'name': "에그 펌", 'desc': '부드러운 곡선과 자연스러운 볼륨감을 강조한 웨이브 펌으로, 달걀형 얼굴처럼 부드럽고 둥근 느낌을 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/heart/egg_perm.jpg'}
 ]
 
 
@@ -71,18 +71,18 @@ oblong_face_recommendations: List[Dict[str, str]] = [
     {'name': "사이드 스위프트", 'desc': '한쪽으로 가벼운 스위프트를 주면 얼굴의 각도를 조절하면서도 동시에 매력적인 스타일을 연출할 수 있습니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/side_swift.jpg'},
     {'name': "소프트 웨이브", 'desc': '부드러운 웨이브를 넣어서 얼굴의 각도를 부드럽게 만들어주고, 전체적인 룩을 부드럽고 여성스럽게 만들어줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/soft_wave.jpg'},
     {'name': "롱 보브", 'desc': '길이가 있는 보브 컷은 머리카락이 얼굴을 감싸면서 얼굴을 좀 더 슬림하게 보이게 해줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/long_bob.jpg'},
-    {'name': "벌룬 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/balloon_perm.jpg'},
-    {'name': "빌드 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/build_perm.jpg'},
-    {'name': "다이앤 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/diann_perm.jpg'},
-    {'name': "그레이스 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/grace_perm.jpg'},
-    {'name': "허쉬 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/hush_cut.jpg'},
-    {'name': "미로 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/miro_perm.jpg'},
-    {'name': "모드 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/mods_perm.jpg'},
-    {'name': "모드 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/mods_cut.jpg'},
-    {'name': "레인 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/rain_perm.jpg'},
-    {'name': "르네 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/rene_perm.jpg'},
-    {'name': "샌드 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/sand_cut.jpg'},
-    {'name': "윈드 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/wind_perm.jpg'}
+    {'name': "벌룬 펌", 'desc': '풍성하고 볼륨감 있는 웨이브를 연출하는 헤어스타일로, 얼굴을 작아 보이게 하고 부드러운 이미지를 강조합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/balloon_perm.jpg'},
+    {'name': "빌드 펌", 'desc': '볼륨을 높이고 곱슬을 부여하여 머리카락을 더 풍성하고 우아하게 연출하는 헤어스타일입니다. 긴 머리에서부터 짧은 머리까지 다양한 길이와 스타일에 적용할 수 있습니다', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/build_perm.jpg'},
+    {'name': "다이앤 펌", 'desc': '부드럽고 우아한 곱슬을 연출하는 헤어스타일로, 풍성한 볼륨과 우아한 웨이브가 특징입니다. 자연스러운 곡선과 부드러운 느낌을 강조하여 로맨틱하고 우아한 분위기를 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/diann_perm.jpg'},
+    {'name': "그레이스 펌", 'desc': '부드럽고 우아한 웨이브를 연출하는 헤어스타일로, 자연스럽게 흐르는 컬이 특징입니다. 이 스타일은 고급스러우면서도 로맨틱한 분위기를 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/grace_perm.jpg'},
+    {'name': "허쉬 컷", 'desc': '부드럽고 자연스러운 곱슬을 연출하는 헤어스타일로, 머리에 볼륨과 우아함을 부여합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/hush_cut.jpg'},
+    {'name': "미로 펌", 'desc': '부드럽고 우아한 곱슬을 연출하는 헤어스타일로, 머리카락을 부드럽게 말아 풍성한 볼륨을 만들어냅니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/miro_perm.jpg'},
+    {'name': "모드 펌", 'desc': '머리카락에 자연스럽고 우아한 웨이브를 연출하는 헤어스타일입니다. 이 스타일은 곱슬을 부드럽게 말아 부드러운 곡선을 형성하며, 풍성한 볼륨과 우아함을 강조합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/mods_perm.jpg'},
+    {'name': "모드 컷", 'desc': '짧게 깎은 머리를 중심으로 모자이크 모양의 스타일을 연출하는 헤어컷입니다. 이 스타일은 일반적으로 양쪽 옆머리와 뒤머리를 짧게 자르고, 중앙 부분을 길게 남겨 화려한 디자인을 만듭니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/mods_cut.jpg'},
+    {'name': "레인 펌", 'desc': '자연스러운 곱슬을 형성하여 머리카락을 길어 보이게 만드는 헤어스타일입니다. 머리카락의 길이와 층을 조절하여 자연스럽고 우아한 웨이브를 만들어냅니다', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/rain_perm.jpg'},
+    {'name': "르네 펌", 'desc': '자연스럽고 볼륨감 있는 웨이브를 연출하는 펌 스타일로, 부드럽고 풍성한 느낌을 주며, 손질이 쉽고 다양한 길이에 어울립니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/rene_perm.jpg'},
+    {'name': "샌드 컷", 'desc': '층을 많이 내어 부드럽고 자연스러운 움직임을 강조하는 컷으로, 가벼운 느낌을 연출하며, 특히 얇은 머리카락에 볼륨감을 더해줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/sand_cut.jpg'},
+    {'name': "윈드 펌", 'desc': '바람에 날리는 듯한 자연스러운 곱슬을 만드는 펌 스타일로, 자유롭고 캐주얼한 느낌을 주며, 자연스러운 분위기를 선호하는 사람들에게 적합합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oblong/wind_perm.jpg'}
 ]
 
 
@@ -103,11 +103,11 @@ square_face_recommendations: List[Dict[str, str]] = [
     {'name': "사이드 스위프트", 'desc': '한쪽으로 가벼운 스위프트를 주면 얼굴의 각도를 조절하면서도 동시에 매력적인 스타일을 연출할 수 있습니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/side_swift.jpg'},
     {'name': "롱 보브", 'desc': '길이가 있는 보브 컷은 머리카락이 얼굴을 감싸면서 얼굴을 좀 더 슬림하게 보이게 해줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/long_bob cut.jpg'},
     {'name': "사이드 파트", 'desc': '측면에 파트를 만들어 얼굴의 길이를 강조하고, 얼굴을 조금 더 길게 보이도록 도와줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/side_part.jpg'},
-    {'name': "비치 웨이브", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/square/beach_wave_hair.jpg'},
-    {'name': "커튼 뱅", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/square/curtain_bangs.jpg'},
-    {'name': "레이어드 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/square/layerd_cut.jpg'},
-    {'name': "프라다 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/square/prada_cut.jpg'},
-    {'name': "스무스 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/square/smooth_cut.jpg'}
+    {'name': "비치 웨이브", 'desc': '해변에서 말린 듯한 자연스럽고 헐렁한 웨이브로, 편안하고 시원한 분위기를 연출하며, 여름철이나 휴양지에서 인기 있는 스타일입니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/beach_wave_hair.jpg'},
+    {'name': "커튼 뱅", 'desc': '얼굴 옆으로 자연스럽게 흘러내리는 긴 앞머리로, 얼굴형을 부드럽게 보완해 주며, 다양한 길이와 스타일에 잘 어울립니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/curtain_bangs.jpg'},
+    {'name': "레이어드 컷", 'desc': '층을 내어 볼륨과 움직임을 주는 컷으로, 다양한 스타일링이 가능하고, 얼굴형에 맞춰 조절할 수 있습니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/layerd_cut.jpg'},
+    {'name': "프라다 컷", 'desc': '깔끔하고 세련된 단발 스타일로, 엣지 있는 도시적인 느낌을 주며, 관리가 쉬워 바쁜 현대인에게 적합합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/prada_cut.jpg'},
+    {'name': "스무스 컷", 'desc': '부드럽고 매끄러운 라인을 강조하는 컷으로, 단정하고 정교한 스타일을 연출하며, 직장이나 공식적인 자리에서도 잘 어울립니다.', 'sex': 'female', 'path': '../image/female_hairstyle/square/smooth_cut.jpg'}
 ]
 
 
@@ -126,13 +126,13 @@ oval_face_recommendations: List[Dict[str, str]] = [
     {'name': "소프트 웨이브", 'desc': '부드러운 웨이브를 넣어서 얼굴의 각도를 부드럽게 만들어주고, 전체적인 룩을 부드럽고 여성스럽게 만들어줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/soft_wave.jpg'},
     {'name': "사이드 파티트", 'desc': '측면에 파트를 만들어 얼굴의 길이를 강조하고, 얼굴을 조금 더 길게 보이도록 도와줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/side_part.jpg'},
     {'name': "레이어드 펌", 'desc': '레이어드 스타일에 파마를 넣어서 얼굴을 더욱 둥글고 부드럽게 만들어줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/layered_perm.jpg'},
-    {'name': "클라우드 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/cloud_perm.jpg'},
-    {'name': "커튼 뱅", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/curtain_bangs.jpg'},
-    {'name': "그레이스 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/grace_perm.jpg'},
-    {'name': "허그 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/hug_cut.jpg'},
-    {'name': "허쉬 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/hush_cut.jpg'},
-    {'name': "샌드 컷", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/sand_cut.jpg'},
-    {'name': "슬릭 펌", 'desc': '', 'sex': 'female', 'path': '../image/female_hairstyle/oval/slick_perm.jpg'},
+    {'name': "클라우드 펌", 'desc': '부드럽고 우아한 웨이브를 연출하는 헤어스타일로, 공중에 떠 있는 구름처럼 부드러운 느낌을 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/cloud_perm.jpg'},
+    {'name': "커튼 뱅", 'desc': '얼굴 옆으로 자연스럽게 흘러내리는 긴 앞머리로, 얼굴형을 부드럽게 보완해 주며, 다양한 길이와 스타일에 잘 어울립니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/curtain_bangs.jpg'},
+    {'name': "그레이스 펌", 'desc': '부드럽고 우아한 웨이브를 연출하는 헤어스타일로, 자연스럽게 흐르는 컬이 특징입니다. 이 스타일은 고급스러우면서도 로맨틱한 분위기를 줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/grace_perm.jpg'},
+    {'name': "허그 컷", 'desc': '얼굴을 감싸는 듯한 느낌을 주는 헤어스타일로, 앞머리를 길게 자르고 옆머리를 짧게 깎아 층을 내어 자연스럽고 부드러운 느낌을 연출합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/hug_cut.jpg'},
+    {'name': "허쉬 컷", 'desc': '부드럽고 자연스러운 곱슬을 연출하는 헤어스타일로, 머리에 볼륨과 우아함을 부여합니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/hush_cut.jpg'},
+    {'name': "샌드 컷", 'desc': '층을 많이 내어 부드럽고 자연스러운 움직임을 강조하는 컷으로, 가벼운 느낌을 연출하며, 특히 얇은 머리카락에 볼륨감을 더해줍니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/sand_cut.jpg'},
+    {'name': "슬릭 펌", 'desc': '매끈하고 윤기 나는 곱슬을 만드는 펌 스타일로, 우아하고 고급스러운 느낌을 주며, 특별한 행사나 중요한 자리에서 돋보이는 스타일입니다.', 'sex': 'female', 'path': '../image/female_hairstyle/oval/slick_perm.jpg'},
 ]
 
 recommendations = {

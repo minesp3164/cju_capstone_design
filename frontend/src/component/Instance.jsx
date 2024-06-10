45d8018423253860d1bfeb5356257e87f5f6cb78
minesp3164 <lhd000721@gmail.com>
2024-06-11 00:15:14 +0900

    feat: http to https

--- frontend/src/component/Instance.jsx
+++ frontend/src/component/Instance.jsx
@@ -1,8 +1,13 @@
 import axios from "axios";
+import https from 'https';
 
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 const axiosServer = axios.create({
   baseURL: 'https://minesp3164.iptime.org:5000',
+  httpsAgent : agent
 })
 
 export default axiosServer;

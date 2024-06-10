cf30585653bc06e6b06404f311d01be5c94c5745
minesp3164 <lhd000721@gmail.com>
2024-06-11 00:09:03 +0900

    feat: axiosServer 수정

--- frontend/src/component/Instance.jsx
+++ frontend/src/component/Instance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosServer = axios.create({
-  baseURL: 'https://minesp3164.iptime.org:5000/',
+  baseURL: 'https://minesp3164.iptime.org:5000',
 })
 
 export default axiosServer;
